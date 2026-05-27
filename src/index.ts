45f2408f95df1463a55c0e67290b3bfa6f678e52
zafarzaeem12 <106338772+zafarzaeem12@users.noreply.github.com>
2026-05-27 17:36:16 +0200

    changing cors

--- src/index.ts
+++ src/index.ts
@@ -22,11 +22,12 @@ console.log(process.env.CLIENT_URL + " client url" + PORT + " port");
 
 app.use(
   cors({
-    // origin: [
-    //  process.env.CLIENT_URL || "http://localhost:3000",
-    //    "http://localhost:3000"],
-    
-    origin: "http://localhost:3000",
+    origin: [
+     process.env.CLIENT_URL as string || "http://localhost:3000" as string,
+       "http://localhost:3000" as string
+      ],
+
+    //origin: "http://localhost:3000",
     credentials: true
   })
 );
